bf430659f65016634d38a263ed59abbb3b0d9d29
Atanasius Surya Gunadharma <220711667@students.uajy.ac.id>
2025-03-30 14:42:02 +0700

    Update data_loader.py

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -7,5 +7,5 @@ def load_data():
     """
     Load and cache the dataset
     """
-    df = pd.read_csv(r"D:\SURYA\UAJY\Semester 6\Pengolahan Bahasa Alami\Pertemuan 6 Praktek Modul 2\modul2_Classification_Text\streamlit_modul2\web\singlelabel\data\train_preprocess.csv")
+    df = pd.read_csv(r"data/train_preprocess.csv")
     return df
