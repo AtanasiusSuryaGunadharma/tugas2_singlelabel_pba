34c874c96aaaedd7419c778bd71f79a6ffc948dc
Atanasius Surya Gunadharma <220711667@students.uajy.ac.id>
2025-04-08 22:20:54 +0700

    Update prediction.py

--- pages/prediction.py
+++ pages/prediction.py
@@ -4,6 +4,20 @@ from utils.preprocessing import preprocess_text
 from utils.visualization import plot_prediction_probabilities
 from models.classifiers import get_classifier, create_vectorizer
 from sklearn.model_selection import train_test_split
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from models.multi_label_classifiers import get_multilabel_classifier, create_vectorizer, evaluate_multilabel_model, create_multilabel_target
+from utils.visualization import plot_multilabel_confusion_matrix
+from streamlit_extras.let_it_rain import rain
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.models import load_model
+from PIL import Image
+from datetime import datetime, timedelta
+import time
+import base64
+from pathlib import Path
+from st_social_media_links import SocialMediaIcons
 
 st.set_page_config(page_title="Prediction", layout="wide")
 st.title("Make Predictions")
@@ -130,3 +144,38 @@ if st.button("Predict"):
         if fig is not None:
             st.subheader("Prediction Probabilities")
             st.pyplot(fig)
+
+def get_base64(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+    
+def set_background(png_file):
+    bin_str = get_base64(png_file)
+    page_bg_img = '''
+    <style>
+    .stApp {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+
+# Menambahkan audio autoplay menggunakan HTML
+try:
+    with open(r"lagu_picapica.mp3", "rb") as audio_file:
+        audio_base64 = base64.b64encode(audio_file.read()).decode()
+
+    audio_html = f"""
+    <audio autoplay loop>
+        <source src="data:audio/mpeg;base64,{audio_base64}" type="audio/mpeg">
+        Your browser does not support the audio element.
+    </audio>
+    """
+    st.markdown(audio_html, unsafe_allow_html=True)
+except FileNotFoundError:
+    st.error("File audio tidak ditemukan. Pastikan 'natal_lagu3.mp3' sudah ada di direktori project.")
+    
+# Change Background Streamlit
+set_background(r"background_music2.gif")
